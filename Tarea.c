acaa92ba1cbd132eddc81bd11962d1c177d73096
DeimHT <DieterDDHT@gmail.com>
2023-11-29 17:28:23 -0300

    A

--- Tarea.c
+++ Tarea.c
@@ -5,13 +5,20 @@
 #include <pthread.h>
 
 #define MAX_TUNEL_CAPACIDAD 1 // Capacidad máxima del túnel
-#define NUM_TUNEL 5 // Cantidad de túneles
+#define NUM_TUNEL 4 // Cantidad de túneles
+#define BUFFER_SIZE 256 // Tamaño del buffer
+
+int entradaAC[2];
+int entradaCB[2];
+int entradaDE[2];
+int entradaED[2];
+
 
 // Estructura para el monitor del túnel
 typedef struct{
     pthread_mutex_t mutex;
-    int capacidad; 
-    int cant_autos; 
+    int capacidad; //capacidad del tunel
+    int cant_autos; //Cantidad de autos que cruzan en el tunel
 }MonitorTunel;
 
 // Estructura para los datos de un automóvil
@@ -20,26 +27,60 @@ typedef struct{
     int prioridad; 
 }Automovil;
 
+// Inicializando el monitor del tunel
+void initMonitorTunel(MonitorTunel *monitor){
+    
+    monitor->capacidad=MAX_TUNEL_CAPACIDAD;
+    monitor->cant_autos=0;
+    pthread_mutex_init(&monitor->mutex, NULL);
+
+}
+
+// Acceso al Monitor
+void accesoMonitor(MonitorTunel *monitor){
+
+    pthread_mutex_lock(&monitor->mutex);
+
+}
+
+// Salida del Monitor
+void salirMonitor(MonitorTunel *monitor){
+
+    pthread_mutex_unlock(&monitor->mutex);
+    
+}
+
+// Función para que el automóvil envíe un mensaje al monitor para entrar al túnel
+void cruzaVehiculoAC(MonitorTunel *monitor, Automovil *Auto) {
+    accesoMonitor(monitor);
+    char msj[] = "Mensaje desde Entrada A";
+}
+
+// Función para que el automóvil notifique al monitor cuando sale del túnel
+void SaleVehiculo(MonitorTunel *monitor, Automovil *Auto) {
+
+}
+
+// Creacion de funciones de los hilos que simulan cada entrada
+
+// Entrada A
+void *hiloEntradaA(MonitorTunel *monitor, Automovil *Auto){
+
+}
+
+
+
+
+
+
+
 // Función para manejar la señal de emergencia
 void emergencia(int adelanto) {
 
 }
 
-// Función para que el automóvil envíe un mensaje al monitor para entrar al túnel
-void EntraVehiculor(MonitorTunel monitor, Automovil Auto) {
 
-}
 
-// Función para que el automóvil notifique al monitor cuando sale del túnel
-void SaleVehiculo(MonitorTunel monitor, Automovil Auto) {
-
-}
-
-// Función ejecutada por cada hilo de automóvil
-void HiloAutomovil(voidarg) {
-    Automovil Auto = (Automovil)arg;
-
-}
 
 int main() {
 
